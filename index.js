c49409e487d2c7cd63f68a024913dc7a3fa80df5
Rahul Ravindran <rahul.ravindran.work@gmail.com>
2018-06-26 21:48:37 -0300

    Promise completed

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-var axios = require('axios');
-var express = require('express');
+const axios = require('axios');
+const express = require('express');
 const cors = require('cors');//Moved here from inside linus post name check
+const { checkEmployee, validateTitleLength,printTitleIfLinus,printAllComments,findFirstPost, delay } = require('./utils')
 
-pi = 3.14;
+const pi = 3.14; //we can use math.pi
 const currentBoss = 'Luke'
 //switched to camel case for yourName
 const yourName = 'John';
 const employees = [ 'James', 'Bobby', 'John' ];
-//Made isMe as arrow function and removed need for if else since triple equals returns a boolean
-// Made isMe as pure by removing dependancy on global
-const checkEmployee =(currentUser) => (employeeName)=> (currentUser === employeeName)
 const isMe = checkEmployee(yourName)
-const checkStringLength = (size) =>  (title) => title && title.length < size
-const validateTitleLength = checkStringLength(50)
-//Restructured the name validation to make it more composable
-const doSomethingIfNameFound = (nameToFind) => (doSomething) => (title) => {
-    (title.indexOf(nameToFind)!==-1) ? doSomething(title)
-        : console.log(`Title doesnt contain the name ${nameToFind}` )
-}
-const doSomethingIfLinus = doSomethingIfNameFound('Linus')
-const printTitleIfLinus = doSomethingIfLinus((title) => console.log('Valid post'))
-const findPostById = (idToFind) => (post) => post.id === idToFind
-const findFirstPost = findPostById(1)
-const printAllComments = (comments) => {
-    console.log(`----------\nComments:`);
-    comments.forEach(({ email, body}) => console.log(`User ${email} wrote:\n${body}\n`));
-  }
 console.log('===== STARTING APPLICATION =====')
 
 // Display the Bosses name
 //Remove need for using this variable
 const printBoss = (bossName) =>  console.log('Dont forget. ' + bossName + ' is the BOSS')
-const p = new Promise((resolve,reject)=> {
-    setTimeout(() => {
-        printBoss(currentBoss)
-        resolve()
-    }, 500);
+delay(500)
+.then(() => {
+    printBoss(currentBoss)
 })
-p.then(() => {
+.then(() => {
     // Display a list of employees
     console.log('Current Employees:');
     //Switched to foreach and used template literal 
@@ -72,7 +53,6 @@ p.then(() => {
     // Lets load the first post and get its comments. Why? I dont know.
     //Moved the if condition to within the response length check because empty response would have caused this to go boom  
     // Also changed the equality check to strict  
-    console.log(data, 'What was obtained')
     const foundPost = data.find(findFirstPost)
     if(!foundPost){
         throw 'Couldnt find post id 1'
@@ -89,13 +69,12 @@ p.then(() => {
 .then(() => {
     console.log('All ajax calls are finished');
 })
-.catch(() => {
-    console.log('Some error occured')
+.catch((e) => {
+    console.log('Some error occured',e)
 })
+.then(() => delay(3000))
 .then(() => {
-    setTimeout(() => {
-        console.log('===== ENDING APPLICATION =====')
-      }, 3000);
+ console.log('===== ENDING APPLICATION =====')
 })
 
 
---
+++ utils.js
@@ -0,0 +1,23 @@
+//Made isMe as arrow function and removed need for if else since triple equals returns a boolean
+// Made isMe as pure by removing dependancy on global
+const checkEmployee =(currentUser) => (employeeName)=> (currentUser === employeeName)
+
+const checkStringLength = (size) =>  (title) => title && title.length < size
+const validateTitleLength = checkStringLength(50)
+//Restructured the name validation to make it more composable
+const doSomethingIfNameFound = (nameToFind) => (doSomething) => (title) => {
+    (title.indexOf(nameToFind)!==-1) ? doSomething(title)
+        : console.log(`Title doesnt contain the name ${nameToFind}` )
+}
+const doSomethingIfLinus = doSomethingIfNameFound('Linus')
+const printTitleIfLinus = doSomethingIfLinus((title) => console.log('Valid post'))
+const findPostById = (idToFind) => (post) => post.id === idToFind
+const findFirstPost = findPostById(1)
+const printAllComments = (comments) => {
+    console.log(`----------\nComments:`);
+    comments.forEach(({ email, body}) => console.log(`User ${email} wrote:\n${body}\n`));
+  }
+const delay = (delayAmount) => new Promise((resolve, reject) => setTimeout(() => resolve(),delayAmount))
+module.exports = {
+    validateTitleLength, checkEmployee, printTitleIfLinus,findFirstPost,printAllComments,delay
+}  
